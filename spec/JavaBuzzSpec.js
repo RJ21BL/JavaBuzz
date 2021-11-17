ae9b94af457634c2183503b702ebe1f42fa6e1eb
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-17 12:03:40 +0000

    Divisible by three test

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,9 +1,17 @@
-describe('JavaBuzz', function() {
+describe('Javabuzz', function() {
   let javabuzz;
 
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
 });
