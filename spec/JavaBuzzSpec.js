8ad68cbece77952aa51a9bf54106e5221b824627
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-17 12:32:34 +0000

    Divisible and not divisible by 15 test and function

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -24,4 +24,16 @@ describe('Javabuzz', function() {
       expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
   });
+  describe('knows when a number is', function () {
+    it('divisible by 15', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
+  describe('knows when a number is NOT', function() {
+    it('divisible by 15', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+  });
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -5,4 +5,7 @@ class Javabuzz {
   isDivisibleByFive(number) {
     return (number % 5 === 0);
   }
+  isDivisibleByFifteen(number) {
+    return (number % 15 === 0);
+  }
 }
